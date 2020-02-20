f99a5b35da8cb12b2a2269875adc1637a43ecbc0
uttie <uttieg@gmail.com>
2020-02-20 14:35:31 -0800

    add node npm and solc

--- dev-nodes/vm_provision.sh
+++ dev-nodes/vm_provision.sh
@@ -32,3 +32,10 @@ sudo usermod -aG docker $USER
 # install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# install node and npm
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install solc
+npm install -g solc
