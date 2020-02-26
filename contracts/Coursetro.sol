fff082d2f6f327cb3703fdd14b51855a8e588c59
aenygma <uttieg@gmail.com>
2020-02-26 09:20:22 +0000

    add a test contract

--- contracts/Coursetro.sol
+++ contracts/Coursetro.sol
@@ -1,17 +1,17 @@
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.25 <=0.6.3;
 
 contract Coursetro {
     
    string fName;
-   uint age;
+   public uint age;
    
-   function setInstructor(string _fName, uint _age) public {
+   function setInstructor(string memory _fName, uint _age) public {
        fName = _fName;
        age = _age;
    }
    
-   function getInstructor() public constant returns (string, uint) {
-       return (fName, age);
+   function getInstructor() view public returns (uint age) {
+       return age;
    }
     
 }
