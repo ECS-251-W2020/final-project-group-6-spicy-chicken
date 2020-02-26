74680e63d2f7d8a700fdded0feba902a40ab5d8e
uttie <uttieg@gmail.com>
2020-02-26 09:56:27 -0800

    add tests

--- dev-nodes/web-server/tests.sh
+++ dev-nodes/web-server/tests.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-curl -vv -XPOST "http://localhost:8080/report" -H "Content-Type: application/x-www-form-urlencoded" -d "accountId=23423&latitude=23.2232&longitude=23.232342"
+IP="spicy-chicken.ddns.net"
+
+echo "http://${IP}"
+
+curl -vv -XPOST "http://${IP}/report" -H "Content-Type: application/x-www-form-urlencoded" -d "accountId=11231&latitude=23.2232&longitude=23.232342"
+
+curl -vv -XPOST "http://${IP}/report" -H "Content-Type: application/x-www-form-urlencoded" -d "accountId=21231&latitude=23.2232&longitude=23.232342"
+
+curl -vv -XPOST "http://${IP}/report" -H "Content-Type: application/x-www-form-urlencoded" -d "accountId=23231&latitude=21.2232&longitude=16.232342"
+
+curl -vv -XPOST "http://${IP}/report" -H "Content-Type: application/x-www-form-urlencoded" -d "accountId=23231&latitude=20.2232&longitude=23.33342"
+
+curl -vv -XPOST "http://${IP}/report" -H "Content-Type: application/x-www-form-urlencoded" -d "accountId=23423&latitude=20.2232&longitude=13.232342"
