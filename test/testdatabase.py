5170b59210461789cba5415d598216e9be172afc
LuckyFanfan <fanwenjing9362@gmail.com>
2020-03-03 20:42:55 -0800

    add database test

--- test/testdatabase.py
+++ test/testdatabase.py
@@ -1,10 +1,17 @@
 import unittest
+import os
 import requests as r
 class TestDatabase(unittest.TestCase):
     def test_connect_database(self):
         database = r.get("http://spicy-chicken.ddns.net/list")
         self.assertEqual(database.status_code, 200)
         #print("connect the database, OK")
+    
+    def test_has_item(self):
+        resp = r.get("http://spicy-chicken.ddns.net/list")
+        with open("list.txt","w") as f:
+            f.write(resp.text)
+        self.assertNotEqual(os.path.exists('list.txt') and os.path.getsize('list.txt'), 0)
 
 if __name__ == '__main__':
     log_file = 'log_file_list.txt'
