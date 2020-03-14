d31bfaf097a48b25f471d9e6a6c1538669c77dae
aenygma <uttie@eth-3.c.spicychicken-268718.internal>
2020-03-14 01:52:10 +0000

    rename ethereum prefix to spck

--- dev-nodes/make_ship_go.sh
+++ dev-nodes/make_ship_go.sh
@@ -6,9 +6,11 @@
 # run bootnode
 ./bootnode.sh
 
+sleep 20
 # start miners
 ./runminers.sh
 
+sleep 20
 # start node
 EXPOSE_FLAG="-p 8545:8545 -p 8546:8546" ./runnode.sh n1
 
--- dev-nodes/runminers.sh
+++ dev-nodes/runminers.sh
@@ -6,5 +6,8 @@
 
 
 ./runnode.sh "m1" --syncmode=full --mine --minerthreads=1 --etherbase="0x3590aca93338b0721966a8d0c96ebf2c4c87c544"
+sleep 10
 ./runnode.sh "m2" --syncmode=full --mine --minerthreads=1 --etherbase="0x8cc5a1a0802db41db826c2fcb72423744338dcb0"
+sleep 10
 ./runnode.sh "m3" --syncmode=full --mine --minerthreads=1 --etherbase="0x0000000000000000000000000000000000000001"
+
