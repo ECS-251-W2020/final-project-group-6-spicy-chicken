331cff5e821d1ec0b0737d89825ea225f4f328da
aenygma <uttie@eth-3.c.spicychicken-268718.internal>
2020-03-19 16:30:59 +0000

    fix geth node prefixes

--- dev-nodes/countpeers.sh
+++ dev-nodes/countpeers.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 NODE=$1
-NODE=${NODE:-"node1"}
-CONTAINER_NAME="ethereum-$NODE"
+NODE=${NODE:-"n1"}
+CONTAINER_NAME="spck-$NODE"
 docker exec -ti "$CONTAINER_NAME" geth --exec 'admin.peers.length' attach
--- dev-nodes/showpeers.sh
+++ dev-nodes/showpeers.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 NODE=$1
-NODE=${NODE:-"node1"}
+NODE=${NODE:-"n1"}
 CONTAINER_NAME="spck-$NODE"
 docker exec -ti "$CONTAINER_NAME" geth --exec 'admin.peers' attach
