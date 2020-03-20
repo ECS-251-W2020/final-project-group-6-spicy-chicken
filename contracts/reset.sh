4e87c2c3904a83a603dfccc1e3b1bd5511612397
aenygma <uttie@eth-3.c.spicychicken-268718.internal>
2020-03-20 02:44:35 -0700

    add private key to unlock and use CONTRACT_ADDR

--- contracts/reset.sh
+++ contracts/reset.sh
@@ -2,6 +2,11 @@
 
 PROJ_DIR=/home/uttie/spicy-chicken-repo
 
+# start containers
+cd "$PROJ_DIR/dev-nodes"
+./make_ship_go.sh
+
+# deploy
 cd "${PROJ_DIR}/contracts/truffle"
 truffle compile
 nodejs "${PROJ_DIR}/contracts/unlock.js"
@@ -10,6 +15,7 @@ ADDR=`echo "console.log(Incident.address)" | truffle console --network dev| grep
 
 echo "wait til dag generation is done"
 echo $ADDR
+export CONTRACT_ADDR="$ADDR"
 
 # compile go server
 cd "$PROJ_DIR/dev-nodes/web-server"
@@ -17,3 +23,5 @@ go clean
 go build
 
 # start server
+sudo CONTRACT_ADDR="$ADDR" ./web-server
+
--- contracts/unlock.js
+++ contracts/unlock.js
@@ -1,6 +1,6 @@
-#!/usr/bin/env nodejs
 
 var Web3 = require("web3")
 var web3 = new Web3("http://localhost:8545")
+web3.eth.personal.importRawKey("df504d175ae63abf209bad9dda965310d99559620550e74521a6798a41215f46", "pass")
 web3.eth.personal.unlockAccount("0x8Cc5A1a0802DB41DB826C2FcB72423744338DcB0", "pass", 1000000).then(console.log)
 
